16028fd6d3dddc2774aa7008554a958c305465ab
VargaDoro <varga.dorothea.timea@gmail.com>
2025-01-19 17:46:34 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-import random
-import szin
-import oszthato
-import autom
+import ertekel
 
-szin.elso()
-szamok=[]
-for i in range(50):
-    szamok.append(random.randint(1, 100))
-oszthato.hettelOszthato(szamok)
-auto_lista=autom.beolvas()
-
-'''for i in range(0, len(auto_lista)):
-    print("*******")
-    print(auto_lista[i].nev)
-    print(auto_lista[i].gyartasi_datum)
-    print()'''
-
-autom.kor(auto_lista[0])
-autom.kiir(auto_lista[1])
-autom.flotta(auto_lista)
+ertekel.elso()
