d61b349a00c9e738b49a836c18b2dbe29be3bae4
VargaDoro <varga.dorotea.timea@gmail.com>
2025-01-20 18:14:43 +0100

    jó

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import ertekel
+import random
+import szin
+import oszthato
+import autom
 
-ertekel.elso()
+szin.elso()
+szamok=[]
+for i in range(50):
+    szamok.append(random.randint(1, 100))
+oszthato.hettelOszthato(szamok)
+auto_lista=autom.beolvas()
+
+'''for i in range(0, len(auto_lista)):
+    print("*******")
+    print(auto_lista[i].nev)
+    print(auto_lista[i].gyartasi_datum)
+    print()'''
+
+autom.kor(auto_lista[0])
+autom.kiir(auto_lista[1])
+autom.flotta(auto_lista)
